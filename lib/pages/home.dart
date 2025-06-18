d3140502239b1f8d2517173b13d2b547259b2763
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com>
2025-06-18 20:30:47 -0300

    first steps of setup, setting home page, search bar, icons, background colors

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 class HomePage extends StatelessWidget{
   const HomePage({super.key});
@@ -6,22 +8,78 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold( 
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.black, 
-            borderRadius: BorderRadius.circular(10.0)),
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20, top: 40),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,         
+                ),
+            ),
+          )
+        ],
+      ),
+      );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg',
+          height: 20,
+          width: 20,
           ),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8), 
+          borderRadius: BorderRadius.circular(10.0)),
         ),
+      ),
+        actions: [
+          GestureDetector(
+            onTap: () {} ,
+            child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8), 
+              borderRadius: BorderRadius.circular(10.0) 
+            ),
+            ),
+          ),
+        ],
       );
   }
 }
